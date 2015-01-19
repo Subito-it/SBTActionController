143222335e596952d86bed37f07993a6497244e8
Mouhcine El Amine <mouhcine.elamine@subito.it>
2015-01-19 15:02:33 +0100

    Fresh start for example project
    Signed-off-by: Mouhcine El Amine <mouhcine.elamine@subito.it>

--- Example/SBTActionController/SBTViewController.m
+++ Example/SBTActionController/SBTViewController.m
@@ -15,23 +15,8 @@
  */
 
 #import "SBTViewController.h"
-
-@interface SBTViewController ()
-
-@end
+#import <SBTActionController/SBTActionController.h>
 
 @implementation SBTViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end

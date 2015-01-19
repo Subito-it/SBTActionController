08a4666b46fed2a61b59cbef33056b54cded85d3
Mouhcine El Amine <mouhcine.elamine@subito.it>
2015-01-19 11:50:56 +0100

    Added license
    Signed-off-by: Mouhcine El Amine <mouhcine.elamine@subito.it>

--- Example/SBTActionController/SBTViewController.h
+++ Example/SBTActionController/SBTViewController.h
@@ -1,12 +1,20 @@
-//
-//  SBTViewController.h
-//  SBTActionController
-//
-//  Created by Mouhcine El Amine on 01/15/2015.
-//  Copyright (c) 2014 Mouhcine El Amine. All rights reserved.
-//
+/**
+ * Copyright (C) 2015 Subito.it S.r.l (www.subito.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface SBTViewController : UIViewController
 
